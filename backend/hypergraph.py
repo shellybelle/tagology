1710abee580da7e53952595aa4e7771d6e1b2c98
shellybelle <michelle.lee.tom@gmail.com>
2026-06-13 16:51:15 -0600

    load csvs from urls

--- backend/hypergraph.py
+++ backend/hypergraph.py
@@ -1,23 +1,29 @@
 from rdflib import Graph, Namespace, URIRef, Literal
-from rdflib.namespace import RDF, RDFS
+from rdflib.namespace import RDF
 from pyshacl import validate
-import pandas, re, time
+import pandas, re, time, requests, psutil, os
 
 HYPER_ONTOLOGY_PATH = "./hyper_ontology.ttl"
 TAG = Namespace("https://theknowledgecommons.org/ns/tagology/")
-DAT = Namespace("https://theknowledgecommons.org/tmp/")
+DATA = Namespace("https://theknowledgecommons.org/tmp/")
+DCTERM = Namespace("http://purl.org/dc/terms/")
+
+def print_ram():
+    rss = psutil.Process(os.getpid()).memory_info().rss / (1024**3)
+    print(f"[PERFORMANCE] Current RAM usage: {rss:.2f} GB")
 
 def slug(text: str) -> str:
     s = str(text).strip().lower()
     s = re.sub(r'\s+', '_', s)
-    s = re.sub(r'[^a-z0-9_]', '', s)
+    s = re.sub(r'[^a-z0-9]+', '_', s)
     s = re.sub(r'_+', '_', s)
     return s.strip('_') or "empty"
 
 def load_hyper_ontology(hypergraph: Graph) -> None:
     hypergraph.parse(HYPER_ONTOLOGY_PATH, format="turtle")
     hypergraph.bind("tag", TAG)
-    hypergraph.bind("dat", DAT)
+    hypergraph.bind("data", DATA)
+    hypergraph.bind("dcterm", DCTERM)
 
 def add_isAArc(hypergraph: Graph, tails: list[URIRef], head: URIRef, headLabel: str) -> None:
     arc = TAG[f"isA_{slug(headLabel)}"]
@@ -37,35 +43,35 @@ def add_hasAArc(hypergraph: Graph, tail: URIRef, tailLabel: str, heads: list[URI
 
     hypergraph.add((arc, TAG.hasTail, tail))
 
-def load_from_csv(csv_path: str, csv_class: str, hypergraph: Graph) -> None:
-    df = pandas.read_csv(csv_path, dtype=str).fillna("")
+def load_from_csv(csv_url: str, csv_obj: str, hypergraph: Graph) -> None:
+    df = pandas.read_csv(csv_url, dtype=str, compression='infer').fillna("")
 
-    obj_class = slug(csv_class)
-    obj_class_uri = DAT[obj_class]
+    obj_class = slug(csv_obj)
+    obj_class_uri = DATA[obj_class]
 
     # [CSV object class] is a hypergraph node
     hypergraph.add((obj_class_uri, RDF.type, TAG.HyperNode))
-    hypergraph.add((obj_class_uri, RDFS.label, Literal(csv_class)))
+    hypergraph.add((obj_class_uri, TAG.label, Literal(csv_obj)))
 
     prop_uris = []
     for col in df.columns:
         prop = slug(col)
-        prop_uri = DAT[prop]
+        prop_uri = DATA[prop]
         prop_uris.append(prop_uri)
 
         # [properties(columns)] are hypergraph nodes
         hypergraph.add((prop_uri, RDF.type, TAG.HyperNode))
-        hypergraph.add((prop_uri, RDFS.label, Literal(col)))
+        hypergraph.add((prop_uri, TAG.label, Literal(col)))
 
         tag_uris = []
         for cell in df[col].unique():
             val = slug(cell)
-            tag_uri = DAT[f"tag/{prop}/{val}"]
+            tag_uri = DATA[f"tag/{prop}/{val}"]
             tag_uris.append(tag_uri)
 
             # [tags = property(column):value pairs] are hypergraph nodes
             hypergraph.add((tag_uri, RDF.type, TAG.HyperNode))
-            hypergraph.add((tag_uri, RDFS.label, Literal(cell)))
+            hypergraph.add((tag_uri, TAG.label, Literal(cell)))
         
         # [tags = property(column):value pairs] IS-A [property(column)]
         add_isAArc(hypergraph, tag_uris, prop_uri, prop)
@@ -76,17 +82,18 @@ def load_from_csv(csv_path: str, csv_class: str, hypergraph: Graph) -> None:
     obj_uris = []
     for index, row in df.iterrows():
         obj_label = f"{obj_class}_{index}"
-        obj_uri = DAT[obj_label]
+        obj_uri = DATA[obj_label]
         obj_uris.append(obj_uri)
 
         # [objects(rows)] are hypergraph nodes
         hypergraph.add((obj_uri, RDF.type, TAG.HyperNode))
+        hypergraph.add((obj_uri, DCTERM.source, URIRef(csv_url)))
 
         tag_uris = []
         for col in df.columns:
             prop = slug(col)
             val = slug(row[col])
-            tag_uri = DAT[f"tag/{prop}/{val}"]
+            tag_uri = DATA[f"tag/{prop}/{val}"]
             tag_uris.append(tag_uri)
 
         # all hypergraph nodes should have been added by this point
@@ -96,12 +103,39 @@ def load_from_csv(csv_path: str, csv_class: str, hypergraph: Graph) -> None:
     # [objects(rows)] IS-A [CSV object class]
     add_isAArc(hypergraph, obj_uris, obj_class_uri, obj_class)
 
-def test(csv_path: str, csv_class: str) -> None:
-    start = time.perf_counter()
+def csvs_to_hypergraph(csvs: list[tuple[str, str]]) -> Graph:
 
     hypergraph = Graph()
     load_hyper_ontology(hypergraph)
-    load_from_csv(csv_path, csv_class, hypergraph)
+
+    for csv_url, csv_obj in csvs:
+        r = requests.head(csv_url, allow_redirects=True)
+        r.raise_for_status()
+        size_mb = int(r.headers.get("Content-Length", 0)) / (1024 * 1024)
+        print(f"[STATUS] Loading {csv_url} ({size_mb:.2f} MB)")
+
+        load_from_csv(csv_url, csv_obj, hypergraph)
+
+        print(f"[STATUS] Graph now contains {len(hypergraph):,} triples")
+        print_ram()
+
+    return hypergraph
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+
+    # TODO: USER ENTERED FILES AND CLASSES
+    csvs = [
+        ("http://scdb.wustl.edu/_brickFiles/2025_01/SCDB_2025_01_caseCentered_Citation.csv.zip",
+         "Supreme Court Cases by Citation"),
+        ("http://scdb.wustl.edu/_brickFiles/2025_01/SCDB_2025_01_caseCentered_Docket.csv.zip",
+         "Supreme Court Cases by Docket"),
+        ("http://scdb.wustl.edu/_brickFiles/2025_01/SCDB_2025_01_caseCentered_LegalProvision.csv.zip",
+         "Supreme Court Cases by Provision"),
+        ("http://scdb.wustl.edu/_brickFiles/2025_01/SCDB_2025_01_caseCentered_Vote.csv.zip",
+         "Supreme Court Cases by Provision - Split Votes")]
+
+    hypergraph = csvs_to_hypergraph(csvs)
 
     '''
     # shacl validation of BF-hypergraph
@@ -111,17 +145,15 @@ def test(csv_path: str, csv_class: str) -> None:
         inference="rdfs"
     )
     print(results_text)
+    '''
 
     print(f"Summary:\n  triples: {len(hypergraph)}")
     print("  Hypergraph Nodes: ", len(list(hypergraph.subjects(RDF.type, TAG.HyperNode))))
     print("  IS-A B-Hyperarcs: ", len(list(hypergraph.subjects(RDF.type, TAG.isAArc))))
     print("  HAS-A F-Hyperarcs: ", len(list(hypergraph.subjects(RDF.type, TAG.hasAArc))))
-    '''
 
-    hypergraph.serialize(destination="hypergraph.ttl", format="turtle")
+    hypergraph.serialize(destination="hypergraph.nt", format="nt")
 
     duration = (time.perf_counter() - start) / 60
     print(f"[PERFORMANCE] Graph creation took {duration:.2f} minutes")
-
-if __name__ == "__main__":
-    test("./SCDB/SCDB_2025_01_caseCentered_Citation.csv", "Supreme Court Cases by Citation")
+    print_ram()
